06d21e729e9b76abfcaa466c45b4025458f3f591
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-04-24 10:40:30 -0300

    :sparkles: add page criar produto

---
+++ web/vh-burguer/src/pages/adicionar-produto/index.tsx
@@ -0,0 +1,16 @@
+import ContentAddProduto from "@/components/content-add-prod/ContentAddProduto";
+import Footer from "@/components/footer/Footer";
+import SubHeader from "@/components/sub-header/SubHeader";
+import React from "react";
+
+const AdicionarProduto = () => {
+    return (
+        <>
+            <SubHeader />
+            <ContentAddProduto />
+            <Footer />
+        </>
+    );
+}
+
+export default AdicionarProduto
