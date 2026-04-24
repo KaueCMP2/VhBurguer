72475175f70803ddadbdd05d68c942709e912e81
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-04-24 17:08:12 -0300

    :phone: fix: update on phone resposivity

---
+++ web/vh-burguer/src/pages/adicionar-categoria/index.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
+
+const Categoria = () => {
+    return (
+        <>
+        <Header />
+        
+        <Footer />
+        </>
+    );
+}
+
+export default Categoria;
