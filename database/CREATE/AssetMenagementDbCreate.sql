6852bbc5b1607229d07dd1a0b485d8674e423d83
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-05-03 14:36:13 -0300

    :truck: refactor: updating database code

---
+++ database/CREATE/AssetMenagementDbCreate.sql
@@ -0,0 +1,252 @@
+CREATE DATABASE	AssetMenagementDb
+GO
+
+USE AssetMenagementDb
+GO
+
+CREATE TABLE Cargo
+(
+	CargoId					UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	NomeCargo				VARCHAR(50) NOT NULL UNIQUE
+)
+GO
+
+CREATE TABLE Cidade
+(
+	CidadeId				UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	NomeCidade				VARCHAR(50) NOT NULL,
+	NomeEstado				VARCHAR(50) NOT NULL
+)
+GO
+
+CREATE TABLE Bairro
+(
+	BairroId				UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY, 
+	NomeBairro				VARCHAR(50) NOT NULL,
+	CidadeId				UNIQUEIDENTIFIER NOT NULL,
+
+	CONSTRAINT FK_Bairro_bairroCidade_cidadeId	
+		FOREIGN KEY (CidadeId) 
+			REFERENCES Cidade(CidadeId) ON DELETE CASCADE
+)
+GO
+
+CREATE TABLE Endereco
+(
+	EnderecoId				UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	Logradouro				VARCHAR(100) NOT NULL,
+	Numero					INT NOT NULL,
+	Complemento				VARCHAR(50) NOT NULL,
+	CEP						VARCHAR(10) NOT NULL,
+	BairroId				UNIQUEIDENTIFIER NOT NULL,
+
+		CONSTRAINT FK_Endereco_EnderecoBairro_BairroId
+			FOREIGN KEY (BairroId) 
+				REFERENCES Bairro(BairroId) ON DELETE CASCADE
+)
+GO
+
+CREATE TABLE TipoUsuario
+(
+	TipoUsuarioId			UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	Nome					VARCHAR(100) NOT NULL
+)
+GO
+
+CREATE TABLE Usuario
+(
+	UsuarioId				UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	NIF						VARCHAR(7)  NOT NULL UNIQUE,
+	NomeUsuario				VARCHAR(50)  NOT NULL,
+	RG						VARCHAR(15)  NOT NULL UNIQUE,
+	CPF						VARCHAR(11)  NOT NULL UNIQUE,
+	CarteiraDeTrabalho		VARCHAR(14),
+	Senha					VARBINARY(32) NULL,
+	Email					VARCHAR(150)  NOT NULL UNIQUE,
+	StatusUsuario			BIT DEFAULT 1,
+	PrimeiroAcesso			BIT DEFAULT 1,
+	EnderecoId				UNIQUEIDENTIFIER NOT NULL,
+	TipoUsuarioId				UNIQUEIDENTIFIER NOT NULL,
+	CargoId					UNIQUEIDENTIFIER NOT NULL,
+
+
+	CONSTRAINT FK_Usuario_UsuaroEndereco_EnderecoId	
+		FOREIGN KEY (EnderecoId) REFERENCES Endereco(EnderecoId),
+
+	CONSTRAINT FK_Usuario_TipoUsuario_TipoUsuarioId
+		FOREIGN KEY (TipoUsuarioId) REFERENCES TipoUsuario(TipoUsuarioId),
+
+	CONSTRAINT FK_Usuario_Cargo_CargoId
+		FOREIGN KEY (CargoId) REFERENCES Cargo(CargoId)
+)
+GO
+
+CREATE TABLE Area
+(
+	AreaId					UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	NomeArea				VARCHAR(50)  NOT NULL UNIQUE
+)
+GO
+
+CREATE TABLE Localizacao
+(
+	LocalizacaoId			UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	NomeLocal				VARCHAR(100),
+	LocalSAP				INT,
+	DescricaoSAP			VARCHAR(100),
+	StatusLocalizacao		BIT DEFAULT 1,
+	AreaId					UNIQUEIDENTIFIER,
+
+	CONSTRAINT FK_Localizacao_Area_AreaId 
+		FOREIGN KEY (AreaId) REFERENCES Area(AreaId)
+)
+GO
+
+-- Local Usuario
+CREATE TABLE LocalUsuario(
+	LocalizacaoId	UNIQUEIDENTIFIER,
+	UsuarioId		UNIQUEIDENTIFIER,
+
+	CONSTRAINT PK_LocalUsuario PRIMARY KEY (LocalizacaoId, UsuarioId),
+
+	CONSTRAINT FK_LocalUsuario_Localizacao
+		FOREIGN KEY (LocalizacaoId) REFERENCES Localizacao(LocalizacaoId),
+
+	CONSTRAINT FK_LocalUsuario_Usuario
+		FOREIGN KEY (UsuarioId) REFERENCES Usuario(UsuarioId)
+);
+GO
+
+ALTER TABLE Localizacao
+ALTER COLUMN LocalSap INT NULL
+
+CREATE TABLE StatusPatrimonio
+(
+	StatusPatrimonioId		UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	NomeStatus				NVARCHAR(50) NOT NULL UNIQUE 
+)
+GO
+
+CREATE TABLE Patrimonio
+(
+	PatrimonioId			UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	Denominacao				VARCHAR(MAX),
+	NumeroSerie				VARCHAR(20) NOT NULL,
+	Valor					DECIMAL(10,2) NOT NULL,
+	Imagem					VARCHAR(MAX) NULL,
+	LocalizacaoId			UNIQUEIDENTIFIER NOT NULL,
+	StatusPatrimonioId		UNIQUEIDENTIFIER NOT NULL,
+
+	CONSTRAINT FK_Patrimonio_Localizacao_LocalizacaoId
+		FOREIGN KEY (LocalizacaoId) REFERENCES Localizacao(LocalizacaoId),
+
+	CONSTRAINT FK_Patrimonio_StatusPatrimonioId 
+		FOREIGN KEY (StatusPatrimonioId) REFERENCES StatusPatrimonio(StatusPatrimonioId) 
+)
+GO
+
+CREATE TABLE TipoAlteracao
+(
+	TipoAlteracaoId			UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	NomeTipoAlteracao		NVARCHAR(50) NOT NULL UNIQUE
+)
+GO
+
+CREATE TABLE Log_Patrimonio
+(
+	LogPatrimonioId			UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	DataTranferencia		DATETIME2 (0) NOT NULL,
+	TipoAlteracaoId			UNIQUEIDENTIFIER NOT NULL,
+	StatusPatrimonioId		UNIQUEIDENTIFIER NOT NULL,
+	PatrimonioId			UNIQUEIDENTIFIER NOT NULL,
+	UsuarioId				UNIQUEIDENTIFIER NOT NULL,
+	LocalizacaoId			UNIQUEIDENTIFIER NOT NULL
+
+	CONSTRAINT FK_Patrimonio_TipoAlteracao_TipoAlteracaoId 
+		FOREIGN KEY (TipoAlteracaoId) REFERENCES TipoAlteracao(TipoAlteracaoId),
+
+	CONSTRAINT FK_Patrimonio_StatusPatimonio_StatusPatrimonioId 
+		FOREIGN KEY (StatusPatrimonioId) REFERENCES StatusPatrimonio(StatusPatrimonioId),
+
+	CONSTRAINT FK_Patrimonio_PatrimonioId 
+		FOREIGN KEY (PatrimonioId) REFERENCES Patrimonio(PatrimonioId),
+
+	CONSTRAINT FK_Patrimonio_UsuarioId 
+		FOREIGN KEY (UsuarioId) REFERENCES Usuario(UsuarioId),
+
+	CONSTRAINT FK_Patrimonio_LocalizacaoId 
+		FOREIGN KEY (LocalizacaoId) REFERENCES Localizacao(LocalizacaoId)
+)
+GO
+
+CREATE TABLE StatusTransferencia 
+(
+	StatusTransferenciaId	UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	NomeStatus				VARCHAR(50) NOT NULL UNIQUE
+)
+GO
+
+CREATE TABLE SolicitacaoTransferencia
+(
+	SolicitacaoId			UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY, 
+	DataSolicitacao			DATETIME2 (0) DEFAULT GETDATE(),
+	DataResposta			DATETIME2 (0) NOT NULL,
+	Justificativa			NVARCHAR(MAX) NOT NULL,
+	StatusTransferenciaId	UNIQUEIDENTIFIER NOT NULL,
+	UsuarioSolicitacaoId	UNIQUEIDENTIFIER NOT NULL,
+	UsuarioAprovacaoId		UNIQUEIDENTIFIER NOT NULL,
+	PatrimonioId			UNIQUEIDENTIFIER NOT NULL,
+	LocalizacaoId			UNIQUEIDENTIFIER NOT NULL
+	
+	CONSTRAINT FK_SolicitacaoTranferencia_StatusTransferenciaId 
+		FOREIGN KEY (StatusTransferenciaId) REFERENCES StatusTransferencia(StatusTransferenciaId),
+	
+	CONSTRAINT FK_SolicitacaoTranferencia_UsuarioSolicitacaoId 
+		FOREIGN KEY (UsuarioSolicitacaoId) REFERENCES Usuario(UsuarioId),
+
+	CONSTRAINT FK_SolicitacaoTranferencia_UsuarioAprovacaoId 
+		FOREIGN KEY (UsuarioAprovacaoId) REFERENCES Usuario(UsuarioId),
+
+	CONSTRAINT FK_SolicitacaoTranferencia_PatrimonioId 
+		FOREIGN KEY (PatrimonioId) REFERENCES Patrimonio(PatrimonioId),
+
+	CONSTRAINT FK_SolicitacaoTranferencia_LocalizacaoId 
+			FOREIGN KEY (LocalizacaoId)	REFERENCES Localizacao(LocalizacaoId)
+)
+GO
+
+-- TRIGGER PARA SOFT DELETE DO USUARIO
+CREATE TRIGGER trg_Usuario_SoftDelete
+ON Usuario
+INSTEAD OF DELETE
+AS
+BEGIN
+	UPDATE Usuario
+		SET StatusUsuario = 0
+		WHERE UsuarioId IN (SELECT UsuarioId FROM deleted);
+END
+GO
+
+-- TRIGGER PARA SOFT DELETE DE LOCALIZAÇÃO
+CREATE TRIGGER trg_Local_SoftDelete
+ON Localizacao
+INSTEAD OF DELETE
+AS
+BEGIN
+	UPDATE Localizacao
+		SET StatusLocalizacao = 0
+		WHERE LocalizacaoId IN (SELECT LocalizacaoId FROM deleted);
+END
+GO
+
+-- TRIGGER PARA SOFT DELETE DE PATRIMONIO
+CREATE TRIGGER trg_Patrimonio_SoftDelete
+ON Patrimonio
+INSTEAD OF DELETE
+AS
+BEGIN
+	UPDATE Patrimonio
+		SET	 StatusPatrimonioId = (SELECT StatusPatrimonioId FROM StatusPatrimonio WHERE NomeStatus = 'Inativo')
+		WHERE StatusPatrimonioId IN (SELECT PatrimonioId FROM deleted);
+END
+GO
