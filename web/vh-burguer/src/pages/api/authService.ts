d9c217f62e24ebef1e3f1bd75355643e91af3875
KaueCMP2 <kaue.s.campos@aluno.senai.br>
2026-05-03 16:20:26 -0300

    :wrench: add Auth Token

---
+++ web/vh-burguer/src/pages/api/authService.ts
@@ -0,0 +1,17 @@
+import secureLocalStorage from "react-secure-storage";
+import { api } from "./api"
+
+export async function login(email: string, senha: string) {
+    try {
+        //? Requisição
+        const response = await api.post("Autenticacao/login", { email, senha });
+        console.log("Funcionou");
+
+        const token = response.data.token;
+        secureLocalStorage.setItem("tokenUser", token);
+
+    }
+    catch (error: any) {
+        throw new Error("Emaill ou senha inválidos!");
+    }
+}
